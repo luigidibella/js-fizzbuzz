f9bad071f69f9a7692a298fc75f8ee3da4f4b41c
luigidibella <lykos.omega@gmail.com>
2024-02-22 17:25:26 +0100

    [fixed] condizione fizzbuzz

--- js/script.js
+++ js/script.js
@@ -3,21 +3,21 @@ for(let i = 1; i <= 100; i++){
   const box = document.createElement('div');
   box.className = 'box';
   box.innerHTML = i;
-  if(i % 15 === 0){
+  if(i % 3 === 0 && i % 5 === 0){
+    console.log("FizzBuzz");
     containerInner.append(box);
     box.classList.add('fizzbuzz')
     box.innerHTML = ('fizzbuzz');
-    console.log("FizzBuzz");
   }else if(i % 5 === 0){
+    console.log('Buzz');
     containerInner.append(box);
     box.classList.add('buzz')
     box.innerHTML = 'buzz';
-    console.log('Buzz');
   }else if(i % 3 === 0){
+    console.log('Fizz');
     containerInner.append(box);
     box.classList.add('fizz')
     box.innerHTML = 'fizz';
-    console.log('Fizz');
   }else{
     console.log(i);
     containerInner.append(box);
