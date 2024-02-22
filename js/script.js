c1a1f4f9a3801adca32d6d8f4cca43d8a4f4467c
luigidibella <lykos.omega@gmail.com>
2024-02-22 16:55:07 +0100

    [add] bonus 1 e bonus 2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
+const containerInner = document.getElementById('container-inner');
 for(let i = 1; i <= 100; i++){
+  const box = document.createElement('div');
+  box.className = 'box';
+  box.innerHTML = i;
   if(i % 15 === 0){
+    containerInner.append(box);
+    box.classList.add('fizzbuzz')
+    box.innerHTML = ('fizzbuzz');
     console.log("FizzBuzz");
   }else if(i % 5 === 0){
-    console.log("Buzz");
+    containerInner.append(box);
+    box.classList.add('buzz')
+    box.innerHTML = 'buzz';
+    console.log('Buzz');
   }else if(i % 3 === 0){
-    console.log("Fizz");
+    containerInner.append(box);
+    box.classList.add('fizz')
+    box.innerHTML = 'fizz';
+    console.log('Fizz');
   }else{
     console.log(i);
+    containerInner.append(box);
   }
 }
